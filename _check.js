4dad4c5f73de7c8f7839c8e0be10a877cfe3cd02
Aaron <aaron@example.com>
2026-06-17 08:02:19 +0800

    Remove debug scripts from repo (should be gitignored)

--- _check.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('C:/Users/Administrator/AppData/Roaming/aaronclaw/data/aaronclaw.db', {readonly:true});
-const config = db.prepare("SELECT data FROM kv WHERE ns='config' AND id='main'").get();
-if (config) { const d = JSON.parse(config.data); console.log('Config model:', d.ai?.model, 'port:', d.gateway?.port); }
-else { console.log('No config!'); }
-db.close();
-process.exit(0);
--- start-dev.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d D:\AaronClaw-New
-set ELECTRON_RUN_AS_NODE=
-start "" "node_modules\electron\dist\electron.exe" . --dev
